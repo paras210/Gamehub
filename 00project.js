ea582c4f2fd5aeceedd31c99e395539366bfce83
PARAS MEHTA <97521165+paras210@users.noreply.github.com>
2024-07-01 00:32:30 +0530

    commits

---
+++ 00project.js
@@ -0,0 +1,15 @@
+
+let x=document.getElementById("li1");
+let y=document.getElementById("rules");
+console.log(x);
+console.log(y);
+function change(){
+    y.innerHTML=" hi";
+}
+
+
+
+// function color() {
+//     var element = document.getElementById("myDIV");
+//     element.classList.toggle("rules");
+//  }
